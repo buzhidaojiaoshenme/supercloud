e80186b0b2f47aafca42abc495b0c102166a71d2
ludl4 <ludl4@lenovo.com>
2021-01-13 10:56:08 +0800

    123

--- eureka-server02/src/main/java/com/example/eurekaserver02/EurekaServer02Application.java
+++ eureka-server02/src/main/java/com/example/eurekaserver02/EurekaServer02Application.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package com.example.eurekaserver02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServer02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(EurekaServer02Application.class, args);
 	}
 
 }
