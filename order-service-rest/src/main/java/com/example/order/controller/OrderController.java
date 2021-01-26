2dcf9826f45ccfac9a8cca677ec64e455a8c7b6e
ludl4 <ludl4@lenovo.com>
2021-01-26 17:31:54 +0800

    增加了单独的

--- order-service-rest/src/main/java/com/example/order/controller/OrderController.java
+++ order-service-rest/src/main/java/com/example/order/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.order.controller;
 import com.example.order.pojo.Order;
 import com.example.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @Value("${catalina.base}")
+    private String catalinaBase;
 
     /**
      * 根据主键查询订单-调用商品服务 /product/list
@@ -27,13 +31,14 @@ public class OrderController {
     }
 
     /**
-     * 根据主键查询订单-调用商品服务 /product/listByIds
+     * 测试sleuth调用链路
      *
      * @param id
      * @return
      */
-    @GetMapping("/product/listByIds/{id}")
+    @GetMapping("/listByIds/{id}")
     public Order queryOrderById(@PathVariable("id") Integer id) {
+        System.out.println("日志路径: " + catalinaBase);
         return orderService.queryOrderById(id);
     }
 
