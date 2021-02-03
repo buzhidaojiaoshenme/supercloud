c547ce24173187b28e58e8a0113f6f8cddb3dc88
ludl4 <ludl4@lenovo.com>
2021-02-03 15:11:35 +0800

    增加了kafka

---
+++ hellokafka/src/main/java/com/example/hellokafk/HellokafkApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellokafk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellokafkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellokafkApplication.class, args);
+    }
+
+}
---
+++ hellokafka/src/test/java/com/example/hellokafk/HellokafkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellokafk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellokafkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
